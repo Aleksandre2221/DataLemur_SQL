301c2782a6227e1d6f7dc3cbcd449ec68f544254
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-04-08 10:53:07 +0200

    Create 1. 3-Topping Pizzas.sql

---
+++ Hard/1. 3-Topping Pizzas.sql
@@ -0,0 +1,11 @@
+
+
+
+         -- Approach 1. Using - 
+SELECT 
+  CONCAT(p1.topping_name, ',', p2.topping_name, ',', p3.topping_name) pizza,
+  (p1.ingredient_cost + p2.ingredient_cost + p3.ingredient_cost) total_cost
+FROM pizza_toppings p1
+JOIN pizza_toppings p2 ON p1.topping_name < p2.topping_name 
+JOIN pizza_toppings p3 ON p2.topping_name < p3.topping_name 
+ORDER BY total_cost DESC, pizza;
