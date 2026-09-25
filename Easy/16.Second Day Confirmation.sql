c38ea4485e814ced6356d370c2bfebf610f9ea88
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-09-25 16:01:38 +0200

    Create 16.Second Day Confirmation.sql

---
+++ Easy/16.Second Day Confirmation.sql
@@ -0,0 +1,9 @@
+
+
+        -- Approach 1. Using - INNER JOIN and - WHERE condition -- 
+SELECT e.user_id
+FROM emails e 
+JOIN texts t 
+  ON e.email_id = t.email_id
+WHERE t.signup_action = 'Confirmed'
+  AND t.action_date - e.signup_date = INTERVAL '1 day';
